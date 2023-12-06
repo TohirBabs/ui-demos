4607b37413a3414512013add4349eb18c4d22662
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-06 03:26:59 +0100

    loader

--- src/components/LoaderButton.jsx
+++ src/components/LoaderButton.jsx
@@ -8,13 +8,16 @@ export const LoaderButton = () => {
     setButtonState("loading");
     try {
       const response = await fetch(
-        "https://official-joke-api.appspot.com/jokes/programming/random"
+        "https://official-joke-api.appspot.com/jokes/programming/ranom"
       );
       const result = await response.json();
       setJokeData(result);
+
       setButtonState("success");
+      // toast.success("API call successful!");
     } catch (error) {
       console.error("Error fetching data:", error);
+      // toast.error("Error fetching data. Please try again.");
       setButtonState("error");
     }
   };
@@ -33,21 +36,47 @@ export const LoaderButton = () => {
     }
   };
 
+  const getColor = (buttonState) => {
+    switch (buttonState) {
+      case "normal":
+        return "rgb(37 99 235)";
+      case "loading":
+        return "70%";
+      case "success":
+        return "rgb(22 163 74)";
+      case "error":
+        return "rgb(220 38 38)";
+      default:
+        return "rgb(37 99 235)";
+    }
+  };
+
   const progress = getWidth(buttonState);
+  const color = getColor(buttonState);
 
   return (
-    <button
-      disabled={buttonState === "loading"}
-      onClick={() => fetchJoke()}
-      className=" bg-blue-700 h-12 px-10 rounded-full font-mono text-white   relative overflow-hidden"
-    >
+    <div className="h-screen w-screen flex bg-gray-400 justify-center items-center">
+      <button
+        disabled={buttonState === "loading"}
+        onClick={() => fetchJoke()}
+        className=" bg-white h-20 px-20 shadow rounded-lg font-mono  text-xl font-bold   relative overflow-hidden"
+      >
+        <span className="z-10 text-blue-600 relative">progress button</span>
+        <div
+          style={{
+            width: progress,
+            backgroundColor: color,
+          }}
+          className="absolute bottom-0 left-0  rounded-full   h-1 transition-width duration-700 flex items-center "
+        ></div>
+      </button>
       <div
-        style={{
-          width: progress,
-        }}
-        className="absolute left-0 text-white top-0 bg-gray-200 opacity-50 h-full transition-all duration-700 flex items-center rounded-full"
-      ></div>
-      <span className="z-10 text-white relative">loader button</span>
-    </button>
+        style={{ opacity: buttonState === "error" ? 1 : 0 }}
+        className="absolute top-10 text-xl bg-white rounded-2xl p-4 transition-all font-mono"
+      >
+        ❌ api call unsuccessfull!
+      </div>
+      {/* {buttonState === "success" && } */}
+    </div>
   );
 };
