d8c370ee3e5c95f27792b82feccac61b3afc952f
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-13 02:48:21 +0100

    partners

---
+++ src/components/Bujeti/Partners.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Partners = () => {
+  return (
+    <div className="w-screen py-10 ">
+      <div className="mx-auto w-[95%] text-center text-[#D28B28] max-w-4xl  flex flex-col gap-5 items-center">
+        <p>Africa&#39;s industry leaders trust us. You should too</p>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+};
