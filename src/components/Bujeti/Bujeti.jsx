9460ec8b20c99ec155c9d70f224806d1d254ac8e
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-13 02:48:38 +0100

    bujeti

---
+++ src/components/Bujeti/Bujeti.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Hero } from "./Hero";
+import { Partners } from "./Partners";
+
+const Bujeti = () => {
+  return (
+    <div className="font-poppins">
+      <Hero />
+      <Partners />
+    </div>
+  );
+};
+
+export default Bujeti;
