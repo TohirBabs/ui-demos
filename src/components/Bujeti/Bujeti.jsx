06b73dff208bce727b923882e80af7aa5b151c83
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-14 01:05:30 +0100

    bujeti

--- src/components/Bujeti/Bujeti.jsx
+++ src/components/Bujeti/Bujeti.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Hero } from "./Hero";
 import { Partners } from "./Partners";
+import Features from "./Features";
 
 const Bujeti = () => {
   return (
     <div className="font-poppins">
       <Hero />
-      <Partners />
+      <Features />
     </div>
   );
 };
