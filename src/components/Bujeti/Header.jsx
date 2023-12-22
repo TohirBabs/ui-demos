ce55bc5aa62c2543f1ac3b0b6c566aad472caaff
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-22 14:26:22 +0000

    updates

--- src/components/Bujeti/Header.jsx
+++ src/components/Bujeti/Header.jsx
@@ -1,26 +1,29 @@
+import { useState } from "react";
+
 export const Header = () => {
+  const [navOpen, setNavOpen] = useState(false)
     return (
-      <div className="mx-auto w-full max-w-6xl border-x border-slate-300 sticky top-0  z-10  lg:p-0 lg:bg-[#f7e8d4] bg-[#f7e8d4]">
-        <div className="border-b border-x  border-[#d28b28]/500 mx-auto border-slate-300 w-full lg:px-8 py-3 text-black max-w-5xl  flex flex-col lg:flex-row justify-between h-16 overflow-hidden lg:h-20 items-center gap-5">
+      <div className={`mx-auto w-full max-w-6xl border-x border-slate-300 sticky top-0  z-10  lg:p-0 lg:bg-[#f7e8d4] bg-${navOpen? "white":"[#f7e8d4]"}`}>
+        <div className={`border-b border-x  border-[#d28b28]/500 mx-auto border-slate-300 w-full lg:px-8 py-3 text-black max-w-5xl  flex flex-col lg:flex-row lg:justify-between h-${navOpen?"screen":"16"} transition transition-all overflow-hidden lg:h-20 lg:items-center gap-5`}>
           <div className="flex justify-between w-full lg:w-auto px-3 lg:px-0 items-center">
             <img src="/bujeti.svg" alt="" className="h-10 p-2 px-4 rounded bg-[#bfff0b]"/>
-            <div className="lg:hidden flex flex-col gap-1">
+            <div onClick={() => setNavOpen(!navOpen)} className="lg:hidden flex flex-col gap-1">
             <div className="w-6 h-1 bg-slate-700 "></div>
             <div className="w-6 h-1 bg-slate-700 "></div>
             <div className="w-6 h-1 bg-slate-700 "></div>
 
             </div>
             </div>
-            <nav className="flex  flex-col lg:flex-row  gap-8 text-md">
+            <nav className="flex p-3 lg:p-0 flex-col lg:flex-row  lg:gap-8 gap-5 text-md">
               <a href="/">why bujeti?</a>
               <a href="/">products</a>
               <a href="/">company</a>
               <a href="/">pricing</a>
             </nav>
           
-          <div className="flex flex-col lg:flex-row gap-8">
-            <button className="capitalize">log in</button>
-            <button className="capitalize bg-[#bfff0b] text-black py-2 px-6 rounded-xl border-4 border-black/20 ">
+          <div className="flex flex-col lg:flex-row lg:gap-8 gap-5 p-3 w-max">
+            <button className="capitalize p-2 border-b border-[#bfff0b] ">log in</button>
+            <button className="capitalize bg-[#bfff0b] hidden lg:block text-black py-2 px-6 rounded-xl border-4 border-black/20 ">
               try it today
             </button>
           </div>
