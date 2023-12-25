acd8fda2a5167f8eafaa89bd742e0669de9a25ab
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-25 17:14:25 +0100

    hero

--- src/components/Bujeti/Hero.jsx
+++ src/components/Bujeti/Hero.jsx
@@ -7,11 +7,17 @@ export const Hero = () => {
       width: "1130",
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
-        autoplay: 0
+        autoplay: 0,
       },
     };
 
-    return <YouTube videoId="rtGIrLQ7O5s" opts={opts} className="hidden lg:block mx-auto overflow-hidden rounded-3xl" />;
+    return (
+      <YouTube
+        videoId="rtGIrLQ7O5s"
+        opts={opts}
+        className="hidden lg:block mx-auto overflow-hidden rounded-3xl"
+      />
+    );
   };
 
   const MobileVideo = () => {
@@ -20,43 +26,47 @@ export const Hero = () => {
       width: "370",
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
-        autoplay: 0
+        autoplay: 0,
       },
     };
-    return <YouTube videoId="rtGIrLQ7O5s" opts={opts} className="lg:hidden mx-auto overflow-hidden rounded-lg" />;
-
+    return (
+      <YouTube
+        videoId="rtGIrLQ7O5s"
+        opts={opts}
+        className="lg:hidden mx-auto overflow-hidden rounded-lg"
+      />
+    );
   };
 
-  
-
   return (
-      
-        <div className="mx-auto w-full lg:border  border-slate-300 text-center text-[#D28B28] max-w-6xl  flex flex-col  items-center">
-         <div className="flex flex-col items-center gap-5 max-w-5xl w-full lg:border-x border-slate-300 py-8 lg:py-20">
-         <h2 className="lg:text-[3.6rem] text-[2.8rem]  font-semibold leading-[1.4] w-full ">
-           <div className="lg:border-b border-slate-300"> Financial management </div><div className="border-b border-slate-300">for your African business</div>
-          </h2>
-          <p className="lg:text-lg text-sm lg:w-[70%] w-[88%] text-slate-900">
-            One platform for comprehensive management of all your financial
-            services, from simple accounting all the way to corporate
-            banking
-          </p>
-          <div className="flex w-[88%] lg:justify-center flex-col lg:flex-row lg:gap-8 gap-2 py-2">
-            <button className="capitalize bg-white text-black py-2 px-6 rounded-xl border-4 border-black/20  ">
-              meet an expert
-            </button>
-            <button className="capitalize bg-[#bfff0b] text-black py-2 px-6 rounded-xl border-4 border-black/20 ">
-              try it today
-            </button>
+    <div className="mx-auto w-full lg:border-x  border-slate-300 text-center text-[#D28B28] max-w-6xl  flex flex-col  items-center">
+      <div className="flex flex-col items-center gap-5 max-w-5xl w-full lg:border-x border-slate-300 py-8 lg:py-20">
+        <h2 className="lg:text-[3.6rem] text-[2.8rem] font-poppins  font-semibold leading-[1.4] w-full ">
+          <div className="lg:border-b border-slate-300">
+            Financial management
           </div>
-          
-         </div>
-          <div className=" w-full lg:p-3 p-1 border-y flex justify-center lg:z-30 bg-black/10 backdrop-blur-md border-slate-300">
-            <DesktopVideo/>
-            <MobileVideo/>
+          <div className="border-b border-slate-300">
+            for your African business
           </div>
+        </h2>
+        <p className="lg:text-lg text-sm lg:w-[70%] w-[88%] text-slate-900">
+          One platform for comprehensive management of all your financial
+          services, from simple accounting all the way to corporate banking
+        </p>
+        <div className="flex w-[88%] lg:justify-center flex-col lg:flex-row lg:gap-8 gap-2 py-2">
+          <button className="capitalize bg-white text-black py-2 px-6 rounded-xl border-4 border-black/20  ">
+            meet an expert
+          </button>
+          <button className="capitalize bg-[#bfff0b] text-black py-2 px-6 rounded-xl border-4 border-black/20 ">
+            try it today
+          </button>
         </div>
-      // <Partners />
-    
+      </div>
+      <div className=" w-full lg:p-3 p-1 border-y flex justify-center  bg-black/10 backdrop-blur-md border-slate-300">
+        <DesktopVideo />
+        <MobileVideo />
+      </div>
+    </div>
+    // <Partners />
   );
 };
