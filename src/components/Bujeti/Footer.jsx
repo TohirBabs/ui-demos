24b3bec71a88b79b35fab8d1bc6da808355aec8f
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-12-28 02:01:27 +0100

    footer

--- src/components/Bujeti/Footer.jsx
+++ src/components/Bujeti/Footer.jsx
@@ -1,210 +1,77 @@
 export const Footer = () => {
+  const linkList = [
+    {
+      title: "products",
+      list: [
+        "expense management",
+        "corporate cards",
+        "bank payments",
+        "reimbursements",
+        "reporting & insights",
+        "fund collection",
+        "bank sync",
+      ],
+    },
+
+    {
+      title: "solutions",
+      list: [
+        "for startups",
+        "for employees",
+        "for finance teams",
+        "for travel & hospitality",
+        "for technology",
+        "for healthcare",
+        "for marketing",
+        "for restaurants",
+        "for non-profits",
+      ],
+    },
+    {
+      title: "company",
+      list: ["about us", "contact us", "careers", "pricing"],
+    },
+    {
+      title: "resources",
+      list: ["  FAQ ", "blog", "help center", "terms of use", "privacy policy"],
+    },
+    {
+      title: "using bujeti",
+      list: ["linkedin", "x.com", "instagram", "youtube"],
+    },
+  ];
   return (
     <div className=" flex flex-wrap lg:justify-between bg-[#D28B28] text-white lg:p-12 p-5 lg:gap-5 gap-10 lg:py-20 py-10 text-xs lg:rounded-t-[3rem] rounded-t-[2rem]">
       <div className="flex flex-col text-sm gap-8 max-w-[250px]">
-        <img src="/bujeti.svg" alt="" className="h-10 w-max" />
+        <div className="flex justify-between w-full lg:w-auto lg:px-0 items-center">
+          <img
+            src="/bujeti.svg"
+            alt=""
+            className="h-10 p-2 px-4 rounded bg-[#bfff0b]"
+          />
+        </div>
         <p>Plot 1B, Block 129, Jide Sawyerr Drive, Lekki Phase 1, Lagos.</p>
         <p>Talk to a product expert today, use our chat box,email us</p>
       </div>
-      <div className="flex flex-col gap-4 capitalize min-w-[150px]">
-        <p className="text-base font-semibold">products</p>
-        <div className="flex flex-col gap-3 ">
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            expense management
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            corporate cards
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            bank payments
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            reimbursements
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            reporting & insights
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            fund collection
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            bank sync
-          </a>
+      {linkList.map((links, index) => (
+        <div
+          key={index}
+          className="flex flex-col gap-4 capitalize min-w-[150px]"
+        >
+          <p className="text-base font-semibold text-black">{links.title}</p>
+          <div className="flex flex-col gap-3 ">
+            {links.list.map((link, index) => (
+              <a
+                key={index}
+                href="/"
+                className="text-slate-200 hover:text-white hover:font-semibold"
+              >
+                {link}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col gap-4 capitalize min-w-[150px]">
-        <p className="text-base font-semibold">solutions</p>
-        <div className="flex flex-col gap-3 ">
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for startups
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for employees
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for finance teams
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for travel & hospitality
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for technology
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for healthcare
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for marketing
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for restaurants
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            for non-profit
-          </a>
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 capitalize min-w-[150px]">
-        <p className="text-base font-semibold">company</p>
-        <div className="flex flex-col gap-3 ">
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            about us
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            contact us
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            careers
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            pricing
-          </a>
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 capitalize min-w-[150px]">
-        <p className="text-base font-semibold">resources</p>
-        <div className="flex flex-col gap-3 ">
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            FAQ
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            blog
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            help center
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            terms of use
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            privacy policy
-          </a>
-        </div>
-      </div>
-      <div className="flex flex-col gap-4 capitalize min-w-[150px]">
-        <p className="text-base font-semibold">using bujeti</p>
-        <div className="flex flex-col gap-3 ">
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            linkedin
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            X.com
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            instagram
-          </a>
-          <a
-            href="/"
-            className="text-slate-200 hover:text-white hover:font-semibold"
-          >
-            youtube
-          </a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
