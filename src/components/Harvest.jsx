0cf4e3869a8e0b12f9bb3e2d0ab9b19ad7415c27
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2024-01-11 15:22:37 +0000

    assets and update

--- src/components/Harvest.jsx
+++ src/components/Harvest.jsx
@@ -5,7 +5,7 @@ const Harvest = () => {
         <div className="absolute left-[25%] top-0">
         <h2 className="relative leading-[5rem]  p-4 text-6xl capitalize">nurturing nature, <br/> harvesting excelence</h2>
         <p className="relative left-[22%] max-w-[60%] p-2">At HarvestGrove, we are passionate about agriculture and committed to providing the highest quality produce and services to our customers. With a legacy of farming excellence spanning 10 years, </p>
-        
+        <img src="/customers.jpg" alt="" className="relative left-[45%] pt-14"/>
         </div>
         </div>
     </div>)
