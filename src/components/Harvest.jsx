95c2e5cdc90ad894303c07a8063d912fd0d507d8
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2024-01-11 15:18:41 +0000

    updates

--- src/components/Harvest.jsx
+++ src/components/Harvest.jsx
@@ -1,6 +1,13 @@
 const Harvest = () => {
-    return (<div>
-        <img src="/grove.svg" alt="" />
+    return (<div className="p-4 font-poppins">
+        <div className="max-w-7xl mx-auto relative">
+        <img src="/grove.svg" alt="" className=""/>
+        <div className="absolute left-[25%] top-0">
+        <h2 className="relative leading-[5rem]  p-4 text-6xl capitalize">nurturing nature, <br/> harvesting excelence</h2>
+        <p className="relative left-[22%] max-w-[60%] p-2">At HarvestGrove, we are passionate about agriculture and committed to providing the highest quality produce and services to our customers. With a legacy of farming excellence spanning 10 years, </p>
+        
+        </div>
+        </div>
     </div>)
 }
 
